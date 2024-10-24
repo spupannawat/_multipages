9c6cd0f35972292d36e471ff52e4fd8c0db405dc
spupannawat <legal2560359@gmail.com>
2024-10-24 23:26:01 +0700

    delete source code

--- src/data/users.jsx
+++
@@ -1,8 +0,0 @@
-const users = [
-    { user: 'user', pass: 'pass', role: 'admin', token: 'user' }
-];
-
-export function verifyUser(user, pass) {
-    const userFound = users.find((u) => u.user === user && u.pass === pass);
-    return userFound ? { role: userFound.role, token: userFound.token } : null;
-}
